555bccb702e5d57c06a20a9b85b17e9518844573
James Roth <roth.jame@northeastern.edu>
2025-02-13 14:35:15 -0500

    a3 halfway thru labs

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,60 @@
+import BooleanVariables from "./BooleanVariables";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOuputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturns from "./ImpliedReturns";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunctions";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
+
 export default function Lab3() {
     return (
-        <div>
-            <h2>Lab 3</h2>
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturns />
+            <TemplateLiterals />
+            <SimpleArrays />
+            <AddingAndRemovingToFromArrays />
+            <ForLoops />
+            <MapFunction />
+            <FindFunction />
+            <FindIndex />   
+            <FilterFunction />
+            <JsonStringify />
+            <House />
+            <TodoItem />
+            <TodoList />
+            <Spreading />
+            <Destructing />
+            <FunctionDestructing />
+            <DestructingImports />
         </div>
     );
 }
